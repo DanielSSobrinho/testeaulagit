2aa9400ee85a3e0ff92a6f5e9391c05df5a722fd
DANIEL SANTOS SOBRINHO <daniel.ssobrinho@senacsp.edu.br>
2022-02-09 11:53:06 -0300

    Front-End Cadastro

--- view/cadusuario.php
+++ view/cadusuario.php
@@ -2,8 +2,57 @@
 include_once("header.php");
 ?>
 
-
-Conteúdo aqui....
+<div class="container">
+<form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputNome" class="form-label">Nome</label>
+    <input type="text" class="form-control" id="inputNome" placeholder="Júlio Naghaita">
+  </div>
+  <div class="col-md-6">
+    <label for="inputEmail" class="form-label">Email</label>
+    <input type="email" class="form-control" id="inputEmail" placeholder="julionaghaita@gmail.com">
+  </div>
+  <div class="col-4">
+    <label for="inputtelefone" class="form-label">Telefone</label>
+    <input type="text" class="form-control" id="inputtelefone" placeholder="(11) 4002-8922">
+  </div>
+  <div class="col-4">
+    <label for="inputcpf" class="form-label">CPF</label>
+    <input type="text" class="form-control" id="inputcpf" placeholder="965.569.965-55">
+  </div>
+  <div class="col-md-4">
+    <label for="inputTipo" class="form-label">Tipo de Usuário</label>
+    <select id="inputTipo" class="form-select">
+      <option selected>Escolha...</option>
+      <option value="1">Funcionário</option>
+      <option value="2">Cliente</option>
+    </select>
+  </div>
+  <div class="col-md-3">
+    <label for="inputCEP" class="form-label">CEP</label>
+    <input type="text" class="form-control" id="inputCEP" placeholder="09787-121">
+  </div>
+  <div class="col-md-3">
+    <label for="inputNumero" class="form-label">Número</label>
+    <input type="text" class="form-control" id="inputNumero" placeholder="220">
+  </div>
+  <div class="col-md-6">
+    <label for="inputComplemento" class="form-label">Complemento</label>
+    <input type="text" class="form-control" id="inputComplemento" placeholder="10A">
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Li os Termos e Estou de Acordo
+      </label>
+    </div>
+  </div>
+  <div class="col-12">
+    <button type="submit" class="btn btn-success">Enviar</button>
+  </div>
+</form>
+</div>
 
 
 <?php
